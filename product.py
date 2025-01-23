203b20e7cb28bcc62260ab089feb4b9f323defe3
Pika Lisa <sashankghimire077@gmail.com>
2025-01-23 22:21:31 +0545

    Add files via upload

--- product.py
+++ product.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import dotenv_values
+
+
+config = dotenv_values(".env")
 
 app = Flask(__name__)
 
 # Database connection parameters
-DB_USERNAME = 'root'  
-DB_PASSWORD = 'database'  
-DB_HOST = '127.0.0.1'        
-DB_PORT = '3306'                # Default MySQL port
-DB_NAME = 'Product'        
+DB_USERNAME = config['DB_USERNAME']
+DB_PASSWORD = config['DB_PASSWORD']
+DB_HOST = config['DB_HOST']
+DB_PORT = config['DB_PORT']
+DB_NAME = config['DB_NAME']        
 
 # Configure MySQL database
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
